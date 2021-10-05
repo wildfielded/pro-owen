b0d9e55f8275ce0ad9ade0ac5d5b9276d6aa433a
Ded Wildfield <wildfielded@gmail.com>
2021-10-05 12:14:45 +0800

    Writing thresholds

--- PoC/main.py
+++ PoC/main.py
@@ -13,10 +13,10 @@ class SensorDataBlock:
         self.sensor_dict = {
             'line_num': 0,
             'place': '',
-            'warn_t': 0,
-            'crit_t': 0,
+            'warn_t': 0.,
+            'crit_t': 0.,
             'status': '',
-            'measures': [ { 'timestamp': 0, 'value': 0 }, ]
+            'measures': [ { 'timestamp': 0., 'value': 0. }, ]
             }
 
     def write_data(self, data_dict: dict = {}):
@@ -31,8 +31,7 @@ class SensorDataBlock:
         if 'status' in data_dict.keys():
             self.sensor_dict['status'] = data_dict['status']
         if 'measures' in data_dict.keys():
-            self.sensor_dict['measures'] = data_dict['measures']
-            #####self.sensor_dict.insert(0, data_dict['measures'])
+            self.sensor_dict['measures'].insert(0, data_dict['measures'][0])
 
     def read_data(self, keys_list: list = []):
         output_dict = {}
@@ -64,88 +63,70 @@ def get_current_files(output_datafile, output_cfgfile, login, passwd, domain,
             s_.retrieveFile(share, cfg_path, g_)
         s_.close()
 
-def create_obj_list(last_file, last_cfg, tz_shift):
-    ''' Создаёт рабочий список объектов класса SensorDataBlock из загруженных
-        файлов с измерениями и пороговыми значениями по каждому датчику с
-        некоторой валидацией данных
+def parse_lastdata(last_file, tz_shift, input_obj_list: list = []):
+    ''' Парсит данные из загруженного файла с измерениями по каждому датчику с
+        некоторой валидацией данных и дополняет текущий (или создаёт новый)
+        список объектов класса SensorDataBlock
     '''
     with open(last_file, 'r', encoding='cp1251') as f_:
         data_list = f_.readlines()
-    with open(last_cfg, 'r', encoding='cp1251') as g_:
-        cfg_list = g_.readlines()
-    output_obj_list = []
-    n_ = 0
-    m_ = 0
-    for data_ in data_list[1:]:
-        n_ += 1
-        data_list = data_.strip().split('\t')
-        data_dict = {
-            'line_num': n_,
-            'place': data_list[2],
-            'measures': [ {}, ]
-            }
-        data_dict['measures'][0]['timestamp'] = time.mktime(time.strptime(data_list[0] + ' ' + data_list[1], '%d.%m.%Y %H:%M:%S')) + tz_shift
-        data_dict['measures'][0]['value'] = float(data_list[3].replace(',', '.'))
-        sensor_obj = SensorDataBlock()
-        sensor_obj.write_data(data_dict)
-        output_obj_list.append(sensor_obj)
-    for conf_ in cfg_list[1:]:
-        m_ += 1
-        cfg_list = conf_.strip().split('\t')
-        cfg_dict = {
-            'line_num': m_,
-            'place': cfg_list[0],
-            'warn_t': float(cfg_list[1]),
-            'crit_t': float(cfg_list[2])
-            }
-        if output_obj_list[m_ - 1].read_data([ 'place' ])['place'] == cfg_dict['place'] and output_obj_list[m_ - 1].read_data([ 'line_num' ])['line_num'] == cfg_dict['line_num']:
-                print(m_)
-                output_obj_list[m_ - 1].write_data(cfg_dict)
+    if len(input_obj_list) == 0:
+        output_obj_list = []
+        n_ = 0
+        for line_ in data_list[1:]:
+            n_ += 1
+            list_ = line_.strip().split('\t')
+            dict_ = {
+                'line_num': n_,
+                'place': list_[2],
+                'measures': [ {}, ]
+                }
+            dict_['measures'][0]['timestamp'] = time.mktime(time.strptime(list_[0] + ' ' + list_[1], '%d.%m.%Y %H:%M:%S')) + tz_shift
+            dict_['measures'][0]['value'] = float(list_[3].replace(',', '.'))
+            sensor_obj = SensorDataBlock()
+            sensor_obj.write_data(dict_)
+            output_obj_list.append(sensor_obj)
+    else:
+        #####!!!!! Заглушка. Нужна обработка переданного списка объектов
+        output_obj_list = input_obj_list
     return output_obj_list
 
-def fill_current_data(last_file, tz_shift, input_obj_list: list = []):
-    ''' Выдирает данные из текущего локального файла измерений и возвращает
-        список объектов класса SensorDataBlock с данными по каждому сенсору
-    '''
-    with open(last_file, 'r', encoding='cp1251') as f_:
-        data_list = f_.readlines()
-    output_obj_list = input_obj_list
-    n_ = 0
-    for l_ in data_list[1:]:
-        n_ += 1
-        line_list = l_.strip().split('\t')
-        dict_ = {
-            'line_num': n_,
-            'place': line_list[2],
-            'measures': [ {}, ]
-            }
-        dict_['measures'][0]['timestamp'] = time.mktime(time.strptime(line_list[0] + ' ' + line_list[1], '%d.%m.%Y %H:%M:%S')) + tz_shift
-        dict_['measures'][0]['value'] = float(line_list[3].replace(',', '.'))
-        sensor_obj = SensorDataBlock()
-        sensor_obj.write_data(dict_)
-        output_obj_list.append(sensor_obj)
-    return output_obj_list
-
-def fill_current_cfg(last_cfg, input_obj_list: list = []):
-    ''' Выдирает данные из текущего локального файла с порогами и возвращает
-        список объектов класса SensorDataBlock с данными по каждому сенсору
+def parse_lastcfg(last_cfg, input_obj_list: list = []):
+    ''' Парсит данные из загруженного файла с пороговыми значениями по каждому
+        датчику с некоторой валидацией данных и дополняет текущий (или создаёт
+        новый) список объектов класса SensorDataBlock
     '''
     with open(last_cfg, 'r', encoding='cp1251') as f_:
         cfg_list = f_.readlines()
-    output_obj_list = input_obj_list
-    n_ = 0
-    for l_ in cfg_list[1:]:
-        n_ += 1
-        line_list = l_.strip().split('\t')
-        dict_ = {
-            'line_num': n_,
-            'place': line_list[0],
-            'warn_t': line_list[1],
-            'crit_t': line_list[2]
-            }
-        sensor_obj = SensorDataBlock()
-        sensor_obj.write_data(dict_)
-        output_obj_list.append(sensor_obj)
+    if len(input_obj_list) == 0:
+        output_obj_list = []
+        n_ = 0
+        for line_ in cfg_list[1:]:
+            n_ += 1
+            list_ = line_.strip().split('\t')
+            dict_ = {
+                'line_num': n_,
+                'place': list_[0],
+                'warn_t': float(list_[1]),
+                'crit_t': float(list_[2])
+                }
+            sensor_obj = SensorDataBlock()
+            sensor_obj.write_data(dict_)
+            output_obj_list.append(sensor_obj)
+    else:
+        output_obj_list = input_obj_list
+        n_ = 0
+        for line_ in cfg_list[1:]:
+            n_ += 1
+            list_ = line_.strip().split('\t')
+            dict_ = {
+                'line_num': n_,
+                'place': list_[0],
+                'warn_t': float(list_[1]),
+                'crit_t': float(list_[2])
+                }
+            if output_obj_list[n_ - 1].read_one('place') == dict_['place'] and output_obj_list[n_ - 1].read_one('line_num') == dict_['line_num']:
+                output_obj_list[n_ - 1].write_data(dict_)
     return output_obj_list
 
 def generate_rows(input_obj_list, row_template):
@@ -157,8 +138,8 @@ def generate_rows(input_obj_list, row_template):
         dict_ = obj_.read_data([ 'place', 'warn_t', 'crit_t', 'measures' ])
         p_ = dict_['place']
         t_ = str(dict_['measures'][0]['value']).replace('.', ',')
-        y_ = dict_['warn_t']
-        r_ = dict_['crit_t']
+        y_ = int(dict_['warn_t'])
+        r_ = int(dict_['crit_t'])
         list_ = time.ctime(dict_['measures'][0]['timestamp']).split()
         m_ = '{} ({} {})'.format(list_[3], list_[2], list_[1])
         row_ = T_(row_template)
@@ -182,8 +163,8 @@ if __name__ == '__main__':
     get_current_files(c_.LAST_DATAFILE, c_.LAST_CFGFILE, c_.LOGIN, c_.PASSWD,
                      c_.DOMAIN, c_.CLI_NAME, c_.SRV_NAME, c_.SRV_IP, c_.SRV_PORT,
                      c_.SHARE_NAME, c_.DATA_PATH, c_.CFG_PATH)
-    current_obj_list = fill_current_data(c_.LAST_DATAFILE, c_.TZ_SHIFT)
-    #####current_obj_list = fill_current_cfg(c_.LAST_CFGFILE, pre_obj_list)
+    current_obj_list = parse_lastdata(c_.LAST_DATAFILE, c_.TZ_SHIFT)
+    current_obj_list = parse_lastcfg(c_.LAST_CFGFILE, current_obj_list)
     tab_rows = generate_rows(current_obj_list, c_.ROW_TEMPLATE)
     write_html(c_.HTML_OUTPUT, c_.HTML_HEADER, c_.HTML_FOOTER, rows=tab_rows)
 
