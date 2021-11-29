fdd7fb4b4c44c46d1532daa468c5ffcb1773f6f6
Ded Wildfield <wildfielded@gmail.com>
2021-11-29 13:43:59 +0800

    Buttons concept works

--- WinWebView/main.py
+++ WinWebView/main.py
@@ -9,6 +9,7 @@ from PyQt5.QtWidgets import (QDesktopWidget, QWidget, QPushButton)
 from PyQt5.QtWidgets import (QHBoxLayout, QVBoxLayout)
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
+import configowen as c_
 
 class OwenWindow(QWidget):
     ''' Основное окно
@@ -23,33 +24,38 @@ class OwenWindow(QWidget):
         win_query_.moveCenter(center_point_)
         self.move(win_query_.topLeft())
 
+    def tune_to(self, server):
+        if server == 1:
+            self.HtmlWidget.load(QUrl(c_.URL_SRV1))
+        if server == 2:
+            self.HtmlWidget.load(QUrl(c_.URL_SRV2))
+
     def setup_main_win(self):
         self.setWindowTitle('OWEN')
         self.setWindowIcon(QIcon('icon-owen.ico'))
         self.resize(700, 800)
-        #####self.resize(self.sizeHint())
         self.move_to_center()
 
-        exit_butt_ = QPushButton(QIcon('icon-exit.svg'), u'Выход')
-        exit_butt_.clicked.connect(QCoreApplication.instance().quit)
+        button_srv1_ = QPushButton(QIcon('icon-downld.svg'), u'Сервер-1')
+        button_srv1_.clicked.connect(lambda: self.tune_to(1))
+        button_srv2_ = QPushButton(QIcon('icon-downld.svg'), u'Сервер-2')
+        button_srv2_.clicked.connect(lambda: self.tune_to(2))
+        button_exit_ = QPushButton(QIcon('icon-exit.svg'), u'Выход')
+        button_exit_.clicked.connect(QCoreApplication.instance().quit)
 
         ToolbarLayout = QHBoxLayout()
         ToolbarLayout.addWidget(QPushButton(QIcon('icon-config.svg'), u'Настройки'))
         ToolbarLayout.addWidget(QPushButton(QIcon('icon-pc.svg'), u'АРМ'))
-        ToolbarLayout.addWidget(QPushButton(QIcon('icon-downld.svg'), u'Сервер-1'))
-        ToolbarLayout.addWidget(QPushButton(QIcon('icon-downld.svg'), u'Сервер-2'))
-        #####ToolbarLayout.addWidget(QPushButton(QIcon('icon-exit.svg'), u'Выход'))
-        ToolbarLayout.addWidget(exit_butt_)
+        ToolbarLayout.addWidget(button_srv1_)
+        ToolbarLayout.addWidget(button_srv2_)
+        ToolbarLayout.addWidget(button_exit_)
 
-        HtmlWidget = QWebEngineView()
-        HtmlWidget.load(QUrl('http://10.130.4.122/owen/'))
-        WebLayout = QVBoxLayout()
-        WebLayout.addWidget(HtmlWidget)
+        self.HtmlWidget = QWebEngineView()
+        self.HtmlWidget.load(QUrl('http://10.130.4.122/owen/'))
 
         main_layout = QVBoxLayout()
         main_layout.addLayout(ToolbarLayout)
-        #####main_layout.addLayout(WebLayout)
-        main_layout.addWidget(HtmlWidget)
+        main_layout.addWidget(self.HtmlWidget)
         self.setLayout(main_layout)
 
 
