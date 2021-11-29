03b0457a0236767205dc9f180e5ba6c0c889a6c5
Ded Wildfield <wildfielded@gmail.com>
2021-11-29 16:42:45 +0800

    Workday save 29-11-2021

--- WinWebView/main.py
+++ WinWebView/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+#####import os
+from os.path import abspath
 import sys
 
 from PyQt5.QtCore import QCoreApplication, QUrl
@@ -25,6 +27,10 @@ class OwenWindow(QWidget):
         self.move(win_query_.topLeft())
 
     def tune_to(self, server):
+        if server == 0:
+            #####self.HtmlWidget.load(QUrl().fromLocalFile('/home/stalk/STALK/GIT/wildfielded/pet-owen/WinWebView/index.html'))
+            #####self.HtmlWidget.load(QUrl().fromLocalFile(os.getcwd() + '/index.html'))
+            self.HtmlWidget.load(QUrl().fromLocalFile(abspath('index.html')))
         if server == 1:
             self.HtmlWidget.load(QUrl(c_.URL_SRV1))
         if server == 2:
@@ -36,16 +42,25 @@ class OwenWindow(QWidget):
         self.resize(700, 800)
         self.move_to_center()
 
+        button_conf_ = QPushButton(QIcon('icon-config.svg'), u'Настройки')
+        button_conf_.setToolTip(u'Настройки программы')
+        button_armd_ = QPushButton(QIcon('icon-pc.svg'), u'АРМ')
+        button_armd_.setToolTip(u'Работа без вэб-сервера')
+        button_armd_.clicked.connect(lambda: self.tune_to(0))
         button_srv1_ = QPushButton(QIcon('icon-downld.svg'), u'Сервер-1')
+        button_srv1_.setToolTip(u'Соединение с основным вэб-сервером')
         button_srv1_.clicked.connect(lambda: self.tune_to(1))
         button_srv2_ = QPushButton(QIcon('icon-downld.svg'), u'Сервер-2')
+        button_srv2_.setToolTip(u'Соединение с резервным вэб-сервером')
         button_srv2_.clicked.connect(lambda: self.tune_to(2))
         button_exit_ = QPushButton(QIcon('icon-exit.svg'), u'Выход')
+        button_exit_.setToolTip(u'Выход из программы')
         button_exit_.clicked.connect(QCoreApplication.instance().quit)
 
         ToolbarLayout = QHBoxLayout()
-        ToolbarLayout.addWidget(QPushButton(QIcon('icon-config.svg'), u'Настройки'))
-        ToolbarLayout.addWidget(QPushButton(QIcon('icon-pc.svg'), u'АРМ'))
+        ToolbarLayout.addWidget(button_conf_)
+        #####ToolbarLayout.addWidget(QPushButton(QIcon('icon-pc.svg'), u'АРМ'))
+        ToolbarLayout.addWidget(button_armd_)
         ToolbarLayout.addWidget(button_srv1_)
         ToolbarLayout.addWidget(button_srv2_)
         ToolbarLayout.addWidget(button_exit_)
