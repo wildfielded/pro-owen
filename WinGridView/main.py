3bfe73d91c7041677a09361f7372a1f9169ee654
Ded Wildfield <wildfielded@gmail.com>
2021-12-22 17:03:49 +0800

    Workday save 22-12-2021

---
+++ WinGridView/main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+import sys
+
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QApplication,
+                             QWidget, QDesktopWidget, QPushButton,
+                             QGridLayout, QHBoxLayout, QVBoxLayout)
+
+#####=====----- Классы -----=====#####
+
+class OwenWindow(QWidget):
+    ''' Основное окно
+    '''
+    def __init__(self):
+        super().__init__()
+        self.setup_main_win()
+
+    def setup_geom(self):
+        ''' Задаёт размер и центрирует окно
+        '''
+        self.resize(800, 800)
+        win_geom_ = self.frameGeometry()
+        win_center_ = QDesktopWidget().availableGeometry().center()
+        win_geom_.moveCenter(win_center_)
+        self.move(win_geom_.topLeft())
+
+    def setup_main_win(self):
+        self.setWindowTitle('OWEN')
+        self.setWindowIcon(QIcon('icon-owen.ico'))
+        self.setup_geom()
+
+        button_exit_ = QPushButton(QIcon('icon-exit.svg'), u'Выход')
+        button_exit_.setToolTip(u'Выход из программы')
+        button_exit_.clicked.connect(QCoreApplication.instance().quit)
+
+        toolbar_layout_ = QHBoxLayout()
+        toolbar_layout_.addWidget(button_exit_)
+
+        datafield_layout_ = QGridLayout()
+
+        main_layout_ = QVBoxLayout()
+        main_layout_.addLayout(toolbar_layout_)
+        main_layout_.addLayout(datafield_layout_)
+        self.setLayout(main_layout_)
+
+
+#####=====----- Функции -----=====#####
+
+#####=====----- Собственно, сама программа -----=====#####
+
+if __name__ == '__main__':
+    app_ = QApplication([])
+    prog_window_ = OwenWindow()
+    prog_window_.show()
+    sys.exit(app_.exec_())
+
+#####=====----- THE END -----=====########################################
