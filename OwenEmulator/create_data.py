eeed318d8dca38a1e4b3d49a7b7ce4e05089e146
Ded Wildfield <wildfielded@wildfielded.site>
2023-06-04 20:50:06 +0800

    Workday save3 04-06-2023

--- OwenEmulator/create_data.py
+++ OwenEmulator/create_data.py
@@ -9,12 +9,18 @@ sys.path.append('../VENVemul/Lib/site-packages')     #####
 from smb.SMBConnection import SMBConnection
 
 from OwenCommon.common_func import (get_current_files, read_json, parse_lastcfg,
-                                    CONF_DICT)
+                                    log_inf, log_err, CONF_DICT)
 
 
 ''' =====----- Функции -----===== '''
 
 def generate_measures():
+    ''' Генерирует значения сенсоров некоторым псевдослучайным способом
+    Arguments:
+        none
+    Returns:
+        none
+    '''
     pass
 
 
@@ -22,9 +28,22 @@ def put_current_files(login: str, passwd: str, domain: str,
                       cli_name: str, srv_name: str,
                       srv_ip: str, srv_port: int,
                       share_name: str, data_path: str, last_datafile: str,
-                      last_datafile_TEST: str, data_path_TEST: str, **kwargs):
+                      **kwargs):
     ''' Записывает файл с генерированными измерениями на сетевой ресурс
     Arguments:
+        login [str] -- Имя учётной записи, под которой идёт обращение на
+            сетевой ресурс сервера OWEN
+        passwd [str] -- Пароль учётной записи
+        domain [str] -- AD-домен учётной записи
+        cli_name [str] -- Имя локальной машины, где выполняется данная
+            программа (можно назвать от балды)
+        srv_name [str] -- NetBIOS/AD-имя сервера OWEN
+        srv_ip [str] -- IP-адрес сервера OWEN
+        srv_port [int] -- Номер TCP-порта для подключения к серверу OWEN
+        share_name [str] -- Имя сетевого ресурса на сервере OWEN
+        data_path [str] -- Путь к текущему файлу данных от корня
+            сетевого ресурса на сервере OWEN
+        last_datafile [str] -- Путь к локальной копии файла данных
     Returns:
         none
     '''
@@ -32,8 +51,9 @@ def put_current_files(login: str, passwd: str, domain: str,
         with SMBConnection(login, passwd, cli_name, srv_name, domain,
                            use_ntlm_v2=True, is_direct_tcp=True) as s_:
             s_.connect(srv_ip, srv_port)
-            with open(last_datafile_TEST, 'rb') as f_:
-                s_.storeFile(share_name, data_path_TEST, f_)
+            with open(last_datafile, 'rb') as f_:
+                s_.storeFile(share_name, data_path, f_)
+            log_inf('OK')
     except:
         log_err('Unable to connect with OWEN server!')
 
@@ -44,6 +64,6 @@ if __name__ == '__main__':
     current_obj_list = parse_lastcfg(read_json(**CONF_DICT), **CONF_DICT)
     print(current_obj_list)
     
-    put_current_files(last_datafile_TEST='logfile.txt', data_path_TEST='Owen/Log_File.txt', **CONF_DICT)
+    # put_current_files(**CONF_DICT)
 
 #####=====----- THE END -----=====#########################################
