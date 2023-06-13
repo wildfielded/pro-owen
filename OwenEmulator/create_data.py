1e5116531b0eddbf3d0aaa1c86604f5be3468a31
Ded Wildfield <wildfielded@wildfielded.site>
2023-06-13 21:34:37 +0800

    Cosmetic fix1 13-06-2023

--- OwenEmulator/create_data.py
+++ OwenEmulator/create_data.py
@@ -63,10 +63,8 @@ if __name__ == '__main__':
     get_result = get_current_files(**CONF_DICT)
     if get_result == 'fresh_data':
         current_obj_list = parse_lastdata(parse_lastcfg(read_json(**CONF_DICT),
-                                                        **CONF_DICT
-                                                       ),
-                                          **CONF_DICT
-                                         )
+                                                        **CONF_DICT),
+                                          **CONF_DICT)
         print(current_obj_list)
     
     # put_current_files(**CONF_DICT)
