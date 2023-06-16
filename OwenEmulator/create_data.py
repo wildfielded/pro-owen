f1d8f3e6c250dbe711f753ed049d4e8a2382af59
Ded Wildfield <wildfielded@wildfielded.site>
2023-06-16 21:19:12 +0800

    Workday save2 16-06-2023 	modified:   OwenEmulator/create_data.py         - Prooved new data generation 	new file:   OwenEmulator/lastdata.csv         - Temporal PoC-file Проверена возможность корректной генерации и записи нового файла с данными через промежуточные файлы. Теперь можно попробовать обновлять данные непосредственно в файле на сетевом ресурсе сервера OWEN.

--- OwenEmulator/create_data.py
+++ OwenEmulator/create_data.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+from time import strftime
+from random import uniform
+
 import sys
 sys.path.append('..')
 #####!!!!! TEMPORAL for VENV working in Windows !!!!!#####
@@ -13,16 +16,51 @@ from OwenCommon.common_func import (inject_config, log_inf, log_err,
                                     parse_lastcfg, parse_lastdata)
 
 
+''' =====----- Переменные и константы -----===== '''
+
+CFG_CSV = 'cfg.csv'
+
 ''' =====----- Функции -----===== '''
 
-def generate_measures():
-    ''' Генерирует значения сенсоров некоторым псевдослучайным способом
+def generate_measure(low_edge: float, high_edge: float) -> float:
+    ''' Генерирует одно значение температуры для сенсора некоторым
+    случайным способом. Сейчас просто берётся случайное число из данного
+    диапазона, но можно прикрутить более продвинутую генерацию.
     Arguments:
-        none
+        low_edge [float] -- Нижний предел диапазона температуры
+        high_edge [float] -- Верхний предел диапазона температуры
     Returns:
-        none
+        [float] -- Значение температуры
     '''
-    pass
+    return round(uniform(low_edge, high_edge), 1)
+
+
+@inject_config()
+def write_measures(login: str, passwd: str, domain: str, cli_name: str,
+                   srv_name: str, srv_ip: str, srv_port: int, **kwargs):
+    ''' Записывает файл с новыми измерениями на сетевой ресурс
+    Arguments:
+        Может принимать весь словарь именованных аргументов.
+        Из них использует:
+        login [str] -- Имя учётной записи, под которой идёт обращение на
+            сетевой ресурс сервера OWEN
+        passwd [str] -- Пароль учётной записи
+        domain [str] -- AD-домен учётной записи
+        cli_name [str] -- Имя локальной машины, где выполняется данная
+            программа (можно назвать от балды)
+        srv_name [str] -- NetBIOS/AD-имя сервера OWEN
+        srv_ip [str] -- IP-адрес сервера OWEN
+        srv_port [int] -- Номер TCP-порта для подключения к серверу OWEN
+    Returns:
+        None
+    '''
+    try:
+        with SMBConnection(login, passwd, cli_name, srv_name, domain,
+                        use_ntlm_v2=True, is_direct_tcp=True) as s_:
+            s_.connect(srv_ip, srv_port)
+            log_inf('Connecting to OWEN server.')
+    except:
+        log_err('Unable to write new data on OWEN server!')
 
 
 @inject_config()
@@ -64,11 +102,12 @@ def put_current_files(login: str, passwd: str, domain: str,
 
 ''' =====----- MAIN -----=====##### '''
 if __name__ == '__main__':
-    get_result = get_current_files()
-    if get_result == 'fresh_data':
-        current_obj_list = parse_lastdata(parse_lastcfg(read_json()))
+    write_measures()
+    # get_result = get_current_files()
+    # if get_result == 'fresh_data':
+        # current_obj_list = parse_lastdata(parse_lastcfg(read_json()))
         # write_json(current_obj_list)
-        print(current_obj_list)
+        # print(current_obj_list)
 
     # put_current_files()
 
