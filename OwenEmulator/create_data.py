e907898a7af908f282c5bbab68e32f051aa9df67
Ded Wildfield <wildfielded@wildfielded.site>
2023-06-17 12:13:57 +0800

    Workday save1 17-06-2023 	modified:   OwenEmulator/create_data.py         - create_measures() works Доведено до рабочего состояния. Не хотелось отказываться от удобства модуля csv, поэтому пришлось отказаться от прямой работы с файлами на сетевом ресурсе и успокоиться на локальных с последующим их копированием на сервер по SMB.

--- OwenEmulator/create_data.py
+++ OwenEmulator/create_data.py
@@ -2,7 +2,6 @@
 
 from time import strftime
 from random import uniform
-from tempfile import SpooledTemporaryFile
 import csv
 
 import sys
@@ -28,65 +27,51 @@ def generate_measure(low_edge: float, high_edge: float) -> float:
         low_edge [float] -- Нижний предел диапазона температуры
         high_edge [float] -- Верхний предел диапазона температуры
     Returns:
-        [float] -- Значение температуры
+        [float] -- Значение температуры с округлением до 0.1
     '''
     return round(uniform(low_edge, high_edge), 1)
 
 
 @inject_config()
-def write_measures(login: str, passwd: str, domain: str, cli_name: str,
-                   srv_name: str, srv_ip: str, srv_port: int, share_name: str,
-                   data_path: str, cfg_path: str, **kwargs):
-    ''' Записывает файл с новыми измерениями на сетевой ресурс
+def create_measures(last_datafile: str, last_cfgfile: str, **kwargs):
+    ''' Записывает файл с новыми измерениями в локальный файл
+    с сохранением формата и структуры данных.
     Arguments:
         Может принимать весь словарь именованных аргументов.
         Из них использует:
-        login [str] -- Имя учётной записи, под которой идёт обращение на
-            сетевой ресурс сервера OWEN
-        passwd [str] -- Пароль учётной записи
-        domain [str] -- AD-домен учётной записи
-        cli_name [str] -- Имя локальной машины, где выполняется данная
-            программа (можно назвать от балды)
-        srv_name [str] -- NetBIOS/AD-имя сервера OWEN
-        srv_ip [str] -- IP-адрес сервера OWEN
-        srv_port [int] -- Номер TCP-порта для подключения к серверу OWEN
-        share_name [str] -- Имя сетевого ресурса на сервере OWEN
-        data_path [str] -- Путь к текущему файлу данных от корня
-            сетевого ресурса на сервере OWEN
-        cfg_path [str] -- Путь к текущему конфигурационному файлу от
-            корня сетевого ресурса на сервере OWEN
+        last_datafile [str] -- Путь к локальной копии файла данных
+        last_cfgfile [str] -- Путь к локальной копии конфигурационного
+            файла
     Returns:
         None
     '''
-    # try:
-    with SMBConnection(login, passwd, cli_name, srv_name, domain,
-                       use_ntlm_v2=True, is_direct_tcp=True) as s_:
-        s_.connect(srv_ip, srv_port)
-        # try:
-        with (SpooledTemporaryFile(mode='wb', newline='',
-                                   encoding='utf-8') as df_,
-              SpooledTemporaryFile(mode='wb', newline='',
-                                   encoding='utf-8') as cf_
+    try:
+        with (open(last_cfgfile, 'r', encoding='utf-8') as cf_,
+              open(last_datafile, 'w', newline='', encoding='utf-8') as df_
              ):
-            s_.retrieveFile(share_name, data_path, df_)
-            s_.retrieveFile(share_name, cfg_path, cf_)
-
             date_list_ = strftime('%d.%m.%Y %H:%M:%S').split()
             date_ = date_list_[0]
             time_ = date_list_[1]
+            # Имена колонок файла данных
             data_cols_ = ['EventDate', 'EventTime', 'Description', 'Value']
 
-            data_dict_ = csv.DictWriter(df_, data_cols_, delimiter='\t')
-            data_dict_.writeheader()
             conf_dict_ = csv.DictReader(cf_, delimiter='\t')
-
-            print(conf_dict_.fieldnames)
-
-            log_inf('Data file updated on OWEN server.')
-        # except:
-            # log_err('Unable to update data file on OWEN server!')
-    # except:
-        # log_err('Unable to connect with OWEN server!')
+            data_dict_ = csv.DictWriter(df_, data_cols_, delimiter='\t')
+            # Запись первой строки файла данных (имена колонок)
+            data_dict_.writeheader()
+            # Построчная запись новых данных в файл
+            for row_ in conf_dict_:
+                writer_dict_ = {
+                    'EventDate': date_,
+                    'EventTime': time_,
+                    'Description': row_['Description'],
+                    'Value': generate_measure(float(row_['Max1']) - 15.0,
+                                              float(row_['Max2']) + 20.0)
+                }
+                data_dict_.writerow(writer_dict_)
+            log_inf('Local data file updated.')
+    except:
+        log_err('Unable to open data and config local files!')
 
 
 @inject_config()
@@ -95,7 +80,8 @@ def put_current_files(login: str, passwd: str, domain: str,
                       srv_ip: str, srv_port: int,
                       share_name: str, data_path: str, last_datafile: str,
                       **kwargs):
-    ''' Записывает файл с генерированными измерениями на сетевой ресурс
+    ''' Копирует локальный файл с генерированными измерениями на сетевой
+    ресурс сервера OWEN.
     Arguments:
         Может принимать весь словарь именованных аргументов.
         Из них использует:
@@ -128,13 +114,13 @@ def put_current_files(login: str, passwd: str, domain: str,
 
 ''' =====----- MAIN -----=====##### '''
 if __name__ == '__main__':
-    write_measures()
+    create_measures()
+    put_current_files()
+
     # get_result = get_current_files()
     # if get_result == 'fresh_data':
         # current_obj_list = parse_lastdata(parse_lastcfg(read_json()))
         # write_json(current_obj_list)
         # print(current_obj_list)
-
-    # put_current_files()
 
 #####=====----- THE END -----=====#########################################
