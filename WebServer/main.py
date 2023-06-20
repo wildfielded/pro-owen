4e5bd22fb2be2b025da70930d732355646c278e5
Ded Wildfield <wildfielded@wildfielded.site>
2023-06-20 22:07:10 +0800

    Cosmetic fix2 20-06-2023

---
+++ WebServer/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+sys.path.append('..')
+#####!!!!! TEMPORAL for VENV working in Windows !!!!!#####
+import sys                                           #####
+sys.path.append('../VENVemul/Lib/site-packages')     #####
+#####!!!!! ------------------------------------ !!!!!#####
+from smb.SMBConnection import SMBConnection
+
+#####=====----- THE END -----=====#########################################
